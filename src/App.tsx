2154cefb56f1ec94a173c46494e718bd88910eae
01ou <Killkas01@gmail.com>
2026-01-11 23:25:28 +0900

    remove create theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
 import MainLayout from "./components/MainLayout";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider>
       <MainLayout />
     </MantineProvider>
   );
